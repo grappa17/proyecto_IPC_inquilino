ec7be593f4bc7de7031d4ff1b51ee7464594d079
grappa17 <loriencs@gmail.com>
2025-12-18 13:06:53 +0100

    Trabajo en el script 7 pasando todos los datos de salarios reales a numeros indice para poder compararlos mejor. De todas formas, esto tiene limitaciones porque el año 2025 no esta completo.

--- 7_Calculo de salarios en base a IPC inquilinos.R
+++ 7_Calculo de salarios en base a IPC inquilinos.R
@@ -30,9 +30,9 @@ ruta_resultados <- file.path(base_dir, "DATOS/RESULTADOS")
 ruta_salarios <- file.path(base_dir, "DATOS/SALARIOS")
 ruta_graficos <- file.path(base_dir, "GRAFICOS")
 
-##### CALCULO SALARIOS REALES 
-# Cargo datos
-salarios_ccaa <- read_xlsx(
+##### CALCULO SALARIOS REALES #####
+# Cargo datos de salarios
+salarios <- read_xlsx(
   path = file.path(ruta_salarios, "coste_laboral_por_hora_efectiva_ccaa.xlsx")
 ) %>%
   pivot_longer(
@@ -49,13 +49,31 @@ salarios_ccaa <- read_xlsx(
     .groups = 'drop'
   )
 
+# Distingo estatales y ccaa
+salarios_ccaa <- salarios[salarios$CCAA != "Total Nacional", ]
 
-deflactores <- readRDS(
+salarios_estatal <- salarios[salarios$CCAA == "Total Nacional", ]
+
+
+# Cargo datos de IPC
+deflactores_ccaa <- readRDS(
   file = file.path(ruta_resultados, "ipc_inquilinos_ccaa.rds")
 ) 
 
+deflactores_estatal <- readRDS(
+  file = file.path(ruta_resultados, "ipc_inquilinos_estatal.rds")
+) 
+
 # Convierto los datos mensuales de IPC en datos anuales haciendo la media anual
-deflactores_anual <- deflactores %>%
+deflactores_anual_ccaa <- deflactores_ccaa %>%
+  group_by(anio, CCAA) %>%
+  summarise(
+    ipc_inquilinos = mean(ipc_inquilinos, na.rm = TRUE),
+    ipc_oficial = mean(ipc_oficial, na.rm = TRUE),
+    .groups = 'drop'
+  )
+
+deflactores_anual_estatal <- deflactores_estatal %>%
   group_by(anio) %>%
   summarise(
     ipc_inquilinos = mean(ipc_inquilinos, na.rm = TRUE),
@@ -63,49 +81,114 @@ deflactores_anual <- deflactores %>%
     .groups = 'drop'
   )
 
-# Convierto anio a numerico en ambos data frames
+
+# Convierto anio a numerico en los data frames
 salarios_ccaa <- salarios_ccaa %>%
   mutate(anio = as.numeric(anio))
 
-deflactores_anual <- deflactores_anual %>%
+salarios_estatal <- salarios_estatal %>%
   mutate(anio = as.numeric(anio))
+
+deflactores_anual_ccaa <- deflactores_anual_ccaa %>%
+  mutate(anio = as.numeric(anio))
+
+deflactores_anual_estatal <- deflactores_anual_estatal %>%
+  mutate(anio = as.numeric(anio))
+
 
 # Uno dfs
-salarios_ccaa <- salarios_ccaa %>%
+salarios_estatal <- salarios_estatal %>%
   left_join(
-    deflactores_anual, by = c("anio")
+    deflactores_anual_estatal, by = c("anio")
   )
 
-# OJO, LOS DEFLACTORES TAMBIEN TENDRIAN QUE SER DATOS POR CCAA, NO LOS GENERALES; ES UN ERROR
+salarios_ccaa <- salarios_ccaa %>%
+  left_join(
+    deflactores_anual_ccaa, by = c("anio", "CCAA")
+  )
 
 
 # Calculo salarios reales
+salarios_estatal <- salarios_estatal %>%
+  mutate(
+    salario_real_oficial = salario / (ipc_oficial / 100),
+    salario_real_inquilinos = salario / (ipc_inquilinos / 100)
+  )
+
+resultados_estatal <- salarios_estatal %>%
+  select(
+    -salario,
+    -ipc_oficial,
+    -ipc_inquilinos,
+    -CCAA
+  )
+
 salarios_ccaa <- salarios_ccaa %>%
   mutate(
     salario_real_oficial = salario / (ipc_oficial / 100),
     salario_real_inquilinos = salario / (ipc_inquilinos / 100)
   )
 
-resultados <- salarios_ccaa %>%
+resultados_ccaa <- salarios_ccaa %>%
   select(
     -salario,
     -ipc_oficial,
     -ipc_inquilinos
   )
 
-# guardo resultados
-resultados_ancho_inquilinos <- resultados %>%
+# Pasar a numeros indice
+# Estatal
+# Obtengo los valores base
+resultados_estatal <- resultados_estatal %>%
+  mutate(
+    salario_real_oficial_base = (salario_real_oficial / salario_real_oficial[anio == anio_base]) * 100,
+    salario_real_inquilinos_base = (salario_real_inquilinos / salario_real_inquilinos[anio == anio_base]) * 100
+  )
+
+# CCAA
+# Obtengo los valores base para cada CCAA
+salarios_base_ccaa <- resultados_ccaa %>%
+  filter(anio == anio_base) %>%
+  select(CCAA, salario_real_oficial, salario_real_inquilinos) %>%
+  rename(
+    salario_oficial_base = salario_real_oficial,
+    salario_inquilinos_base = salario_real_inquilinos
+  )
+
+# Creo indices con base seleccionada = 100 para cada CCAA
+resultados_ccaa <- resultados_ccaa %>%
+  left_join(salarios_base_ccaa, by = "CCAA") %>%
+  mutate(
+    salario_real_oficial_base = (salario_real_oficial / salario_oficial_base) * 100,
+    salario_real_inquilinos_base = (salario_real_inquilinos / salario_inquilinos_base) * 100
+  ) %>%
   select(
-    -salario_real_oficial
+    -salario_oficial_base,
+    -salario_inquilinos_base
+  )
+
+# guardo resultados
+write_xlsx(
+  resultados_estatal,
+  path = file.path(ruta_resultados, "salario_real_inquilinos_y_oficial_estatal.xlsx")
+)
+
+resultados_ancho_inquilinos_ccaa <- resultados_ccaa %>% # Aquí estoy guardando solo los datos normales, sin indice
+  select(
+    -salario_real_oficial,
+    -salario_real_oficial_base, 
+    -salario_real_inquilinos_base 
   ) %>%
   pivot_wider(
     names_from = anio,
     values_from = c(salario_real_inquilinos)
   )
 
-resultados_ancho_oficial <- resultados %>%
+resultados_ancho_oficial_ccaa <- resultados_ccaa %>%
   select(
-    -salario_real_inquilinos
+    -salario_real_inquilinos,
+    -salario_real_inquilinos_base,
+    -salario_real_oficial_base
   ) %>%
   pivot_wider(
     names_from = anio,
@@ -113,38 +196,97 @@ resultados_ancho_oficial <- resultados %>%
   )
   
 write_xlsx(
-  resultados_ancho_inquilinos,
-  path = file.path(ruta_resultados, "salario_real_inquilinos.xlsx_ccaa")
+  resultados_ancho_inquilinos_ccaa,
+  path = file.path(ruta_resultados, "salario_real_inquilinos_ccaa.xlsx")
 )
 
 write_xlsx(
-  resultados_ancho_oficial,
+  resultados_ancho_oficial_ccaa,
   path = file.path(ruta_resultados, "salario_real_oficial_ccaa.xlsx")
 )
 
-# Graficar
-# Filtrar Total Nacional y preparar datos
-datos_plot <- resultados %>%
-  filter(CCAA != "Total Nacional") %>%
-  # # Crear variable temporal continua
-  # mutate(
-  #   periodo = as.numeric(anio) + (as.numeric(trimestre) - 1) / 4
-  # ) %>%
-  # Convertir a formato largo para ggplot
+
+
+
+###### GRAFICAR ######
+
+# ESTATAL
+datos_plot_1 <- resultados_estatal %>%
+  select(
+    -salario_real_oficial,
+    -salario_real_inquilinos
+  ) %>%
   pivot_longer(
-    cols = c(salario_real_oficial, salario_real_inquilinos),
+    cols = c(salario_real_oficial_base, salario_real_inquilinos_base),
     names_to = "tipo_salario",
     values_to = "salario"
   ) %>%
   mutate(
     tipo_salario = case_when(
-      tipo_salario == "salario_real_oficial" ~ "Oficial",
-      tipo_salario == "salario_real_inquilinos" ~ "Inquilinos"
+      tipo_salario == "salario_real_oficial_base" ~ "Oficial",
+      tipo_salario == "salario_real_inquilinos_base" ~ "Inquilinos"
+    )
+  )
+
+# Grafico
+grafico_salarios_estatal <- ggplot(datos_plot_1, aes(x = anio, y = salario, color = tipo_salario, group = tipo_salario)) +
+  geom_line(linewidth = 1) +
+  scale_color_manual(
+    values = c("Oficial" = c_oficial, "Inquilinos" = c_inquilinos),
+    name = "Tipo de IPC"
+  ) +
+  labs(
+    title = "Salario por hora real deflactado con distintos índices de precios",
+    subtitle = "Coste salarial por hora efectiva, Estado español",
+    x = "Año",
+    y = "Salario real por hora",
+    color = "Serie"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(face = "bold", size = 14, hjust = 0.5),
+    legend.position = "bottom"
+  ) +
+  labs(caption = "Fuente: Elaboración propia con datos de la ETCL, el IPC, Idealista (precios del alquiler) \ny la Encuesta de Condiciones de Vida (ponderaciones del alquiler). Autor: Gabinete Socioeconómico CGT (@CGT_economia).")
+
+grafico_salarios_estatal
+
+# Guardar
+ggsave(
+  file.path(ruta_graficos, "comparacion_salarios_estatal.png"),
+  plot = grafico_salarios_estatal,
+  width = 8.27, 
+  height = 11.69, 
+  dpi = 300, 
+  bg = "white"
+)
+
+
+
+
+
+# CCAA
+# Filtrar Total Nacional y preparar datos
+datos_plot_2 <- resultados_ccaa %>%
+  filter(CCAA != "Total Nacional") %>%
+  select(
+    -salario_real_oficial,
+    -salario_real_inquilinos
+  ) %>%
+  pivot_longer(
+    cols = c(salario_real_oficial_base, salario_real_inquilinos_base),
+    names_to = "tipo_salario",
+    values_to = "salario"
+  ) %>%
+  mutate(
+    tipo_salario = case_when(
+      tipo_salario == "salario_real_oficial_base" ~ "Oficial",
+      tipo_salario == "salario_real_inquilinos_base" ~ "Inquilinos"
     )
   )
 
 # Crear gráfico con facetas
-grafico_salarios <- ggplot(datos_plot, aes(x = anio, y = salario, color = tipo_salario, group = tipo_salario)) +
+grafico_salarios_ccaa <- ggplot(datos_plot_2, aes(x = anio, y = salario, color = tipo_salario, group = tipo_salario)) +
   geom_line(linewidth = 1) +
   facet_wrap(~ CCAA, ncol = 3, scales = "free_y") +
   scale_color_manual(
@@ -168,10 +310,13 @@ grafico_salarios <- ggplot(datos_plot, aes(x = anio, y = salario, color = tipo_s
     axis.text.x = element_text(angle = 45, hjust = 1, size = 8)
   )
 
+grafico_salarios_ccaa
+
+
 # Guardar
 ggsave(
   file.path(ruta_graficos, "comparacion_salarios_ccaa.png"),
-  plot = grafico_salarios,
+  plot = grafico_salarios_ccaa,
   width = 8.27, 
   height = 11.69, 
   dpi = 300, 
